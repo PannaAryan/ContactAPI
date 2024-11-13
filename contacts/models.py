85d3f749c74522dffaaee195878754c444b5f1e3
PannaAryan <panna.codebase@gmail.com>
2024-11-13 17:00:28 +0600

    sqlite to postgres

--- contacts/models.py
+++ contacts/models.py
@@ -4,6 +4,8 @@ class Contact(models.Model):
     name = models.CharField(max_length=100)
     number = models.CharField(max_length=15, unique=True)
     email = models.EmailField()
+    address = models.CharField(max_length=100)
+
 
     def __str__(self):
         return self.name
